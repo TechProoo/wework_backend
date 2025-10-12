8ff3e14692549c648f81deb4df587937a198292a
unknown <olaoluwaoyetibo@gmail.com>
2025-10-12 19:49:45 +0100

    Finished student signup

--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,12 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { CreateStudentDto } from './dto/create-student.dto';
+import bcrypt from 'bcrypt';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class StudentsService {
-  create(createStudentDto: CreateStudentDto) {
-    return {
-      message: 'This action adds a new student',
-      student: createStudentDto,
-    };
+  private prisma = new PrismaClient();
+
+  async signUp(createStudentDto: CreateStudentDto) {
+    const { password, email, ...rest } = createStudentDto;
+
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      throw new ConflictException('Email already in use');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const created = await this.prisma.user.create({
+      data: {
+        email,
+        passwordHash: hashedPassword,
+        ...rest,
+      },
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { passwordHash, ...safeUser } = created;
+    return safeUser;
   }
 }
